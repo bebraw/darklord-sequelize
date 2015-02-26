8344ca0c4d4046b77362fee053d2b285b373de41
Juho Vepsalainen <bebraw@gmail.com>
2015-02-26 16:32:55 +0200

    Match reference User model better

--- models/user.js
+++ models/user.js
@@ -3,14 +3,17 @@ var passportLocalSequelize = require('passport-local-sequelize');
 
 
 module.exports = function(sequelize, DataTypes) {
-    // createdAt -> created
-    var User = passportLocalSequelize.defineUser(sequelize, {
+    var User = sequelize.define('User', {
         email: DataTypes.STRING,
         active: DataTypes.BOOLEAN,
         verified: DataTypes.BOOLEAN,
         verifyToken: DataTypes.STRING,
         forgotPasswordToken: DataTypes.STRING,
         forgotPasswordExpires: DataTypes.DATE,
+    });
+
+    passportLocalSequelize.attachToUser(User, {
+        usernameField: 'email',
     });
 
     return User;
