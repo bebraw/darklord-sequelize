07a0794aa947aea256e05b47a74bc72a93f2a716
Juho Vepsalainen <bebraw@gmail.com>
2015-04-01 13:59:12 +0300

    Run server and nuke db before running each test

--- specs/register_spec.js
+++ specs/register_spec.js
@@ -2,17 +2,41 @@
 var jasmine = require('jasmine-node');
 var frisby = require('frisby');
 
+var config = require('../config');
+
+config.database.test.logging = noop;
+
+var models = require('../models')(config.database.test);
+var server = require('../server');
+
 var URL = 'http://localhost:3000';
 
 
-main();
+describe('Registration', function() {
+    var port = 1351;
+    var s = null;
 
-function main() {
-    // TODO: nuke test db + run server before testing
-    register();
-}
+    beforeEach(function(done) {
+        // run server
+        server({
+            config: config,
+            models: models,
+        }, function(app) {
+            s = app.listen(port);
 
-function register() {
+            // nuke possible db
+            models.sequelize.sync({
+                force: true,
+            }, done);
+        });
+    });
+
+    afterEach(function() {
+        s.close();
+    });
+
+    // XXX: figure out this part
+    //it('should be able to register', function() {
     frisby.create('POST register').post(URL + '/register', {
         email: 'myemail@address.com',
         password: '123456',
@@ -26,4 +50,7 @@ function register() {
         console.log('user', user);
     }).
     toss();
-}
+    //});
+});
+
+function noop() {}
